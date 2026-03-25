9d7e16cafe8215263eaa3cee5a659c6647bda336
Juan Sanchez <sanchezgica@gmail.com>
2026-03-24 21:03:31 -0300

    modificado sidebar

--- Teorico/ProyectoReact/src/components/layout/SidebarComponent.jsx
+++ Teorico/ProyectoReact/src/components/layout/SidebarComponent.jsx
@@ -4,7 +4,7 @@ function Sidebar(props) {
   return (
     <div
       className="col-2 px-3 py-3 d-flex flex-column align-items-center
-     border"
+     borde"
       style={{ height: "100vh" }}
     >
       <Tarjeta {...props}></Tarjeta>
