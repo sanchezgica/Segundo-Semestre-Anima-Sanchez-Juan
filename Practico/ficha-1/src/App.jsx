a3d5391ed90431d1eb5726a396146e919f0b27e0
Juan Sanchez <96532433+sanchezgica@users.noreply.github.com>
2026-04-13 18:18:01 -0300

    mergeado conflicto ficha-1

--- Practico/ficha-1/src/App.jsx
+++ Practico/ficha-1/src/App.jsx
@@ -9,40 +9,7 @@ function App() {
     <>
       <section id="center">
         <h1>Ficha 1 - Contador</h1>
-<<<<<<< HEAD
-        <div className="container">
-          <button className="counter" onClick={resta}>
-            -
-          </button>
-          <div className="texto">{mostrar && <p>{count}</p>}</div>
-          <button className="counter" onClick={suma}>
-            +
-          </button>
-          <button className="counter" onClick={cambioMostrar}>
-            {mostrar ? (
-              <span className="container-span" style={{ gap: "4px" }}>
-                <img
-                  className="icon"
-                  src="../src/assets/hidden.png"
-                  alt="hide icon"
-                />
-                <p>Ocultar</p>
-              </span>
-            ) : (
-              <span className="container-span" style={{ gap: "4px" }}>
-                <img
-                  className="icon"
-                  src="../src/assets/eye.png"
-                  alt="show icon"
-                />
-                <p>Mostrar</p>
-              </span>
-            )}
-          </button>
-        </div>
-=======
         <Counter></Counter>
->>>>>>> 3f214d1ce927b83580b4b973ff3110512f6811db
       </section>
     </>
   );
