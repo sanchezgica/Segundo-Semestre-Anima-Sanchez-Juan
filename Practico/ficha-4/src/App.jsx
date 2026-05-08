34c15b5422ef3ef23e9a7ac53c0a42ec6573a00d
Juan Sanchez <96532433+sanchezgica@users.noreply.github.com>
2026-05-07 21:01:05 -0300

    Creado component Header

--- Practico/ficha-4/src/App.jsx
+++ Practico/ficha-4/src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const productos = [{id:1, nombre="x1", precio=1000},{id:2, nombre="x2", precio=1500},{id:3, nombre="x4", precio=2000},{id:1, nombre="x4", precio=1800}];
+
 
   return (
     <>
       <div></div>
     </>
   );
-}
+};
 
 export default App;
---
+++ Practico/ficha-4/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className="header">
+      <button className="cart-btn">Mi Carrito</button>
+    </div>
+  );
+}
+
+export default Header;
