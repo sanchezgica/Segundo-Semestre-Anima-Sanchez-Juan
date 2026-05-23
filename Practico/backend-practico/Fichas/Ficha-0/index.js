45c5c7d1fcc4271ae25438db5d860fc39a3ac33b
Juan Sánchez <sanchezgica@gmail.com>
2026-05-22 22:05:31 -0300

    modificado index.js

--- Practico/backend-practico/Fichas/Ficha-0/index.js
+++ Practico/backend-practico/Fichas/Ficha-0/index.js
@@ -1,13 +1,28 @@
 const plantas = [
-  { id: 1, tipo: "Lechuga" },
-  { id: 2, tipo: "Albahaca" },
-  { id: 3, tipo: "Espinaca" },
+  { id: 1, nombre: "Lechuga" },
+  { id: 2, nombre: "Albahaca" },
+  { id: 3, nombre: "Espinaca" },
 ];
 
 function listarPlantas() {
   console.log(plantas);
 }
 
-function agregarPlanta(tipo) {
-  plantas.push({ id: plantas.length + 1, tipo: tipo });
+function agregarPlanta(nombre) {
+  plantas.push({ id: plantas.length + 1, nombre: nombre });
+}
+
+agregarPlanta("Menta");
+listarPlantas();
+
+function buscarPlanta(id) {
+  return plantas.find((planta) => planta.id === id);
+  console.log(buscarPlanta);
+}
+
+function modificarPlanta(id, nuevoNombre) {
+  const planta = buscarPlanta(id);
+  if (planta) {
+    planta.nombre = nuevoNombre;
+  }
 }
