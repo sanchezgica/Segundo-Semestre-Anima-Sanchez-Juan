1183670ab91c36f639471bf4c60adcb34e78b477
Juan Sanchez <96532433+sanchezgica@users.noreply.github.com>
2026-05-25 19:23:24 -0300

    actualizado a ficha-1 parte 7

--- Practico/backend-practico/Fichas/Ficha-0/index.js
+++ Practico/backend-practico/Fichas/Ficha-0/index.js
@@ -37,6 +37,21 @@ app.get("/plantas", (req, res) => {
   res.json(plantas);
 });
 
+app.get("/", (req, res) => {
+  res.send("Bienvenido al backend de plantas");
+});
+
+app.get("/plantas/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const planta = plantas.find((planta) => planta.id === id);
+  console.log("planta buscada" + planta);
+  if (planta) {
+    res.json(planta);
+  } else {
+    res.json({ mensaje: "Planta no encontrada" });
+  }
+});
+
 app.listen(3000, () => {
   console.log("Servidor corriendo en localhost puerto:3000");
 });
